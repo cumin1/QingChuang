2a5f08c3c948fd1caa7e7de19fbded15e58d21f7
cumin1 <113490377+cumin1@users.noreply.github.com>
2023-07-10 14:22:08 +0800

    test

--- 第四批230710/网络暴力检测.py
+++ 第四批230710/网络暴力检测.py
@@ -19,16 +19,24 @@ print('Token:', token)
 def main():
     url = "https://aip.baidubce.com/rest/2.0/solution/v1/text_censor/v2/user_defined?access_token=" + \
         login_baidu_ai(ak, sk)
-
-    payload = 'text=' + urlencode(input("请输入一句话"))
+    pword = input("请输入一句话:")
+    payload = urlencode({"text": pword})
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
+    try:
+        sword = response.json()[
+            "data"][0]["hits"][0]["wordHitPositions"][0]["keyword"]
 
-    print(response.text)
+        print("检测到敏感文本:  "+sword)
+        lens = len(sword)
+        a = pword.replace(sword, "*"*lens)
+        print("替换后的文本:  "+a)
+    except:
+        print("该文本没有网络暴力词汇")
 
 
 if __name__ == '__main__':
